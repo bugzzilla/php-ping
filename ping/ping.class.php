f1fe4fa3f224e1619699b31812877406e20d2dab
Anton Petrov <bugzzilla@gmail.com>
2016-08-04 13:15:50 +0500

    rep update

--- ping/ping.class.php
+++ ping/ping.class.php
@@ -1,6 +1,14 @@
 <?php
 
-
 	class ping {
+		
+		public $pingResult = array (
+							'rawStdout' => '', 
+							'returnVar' => -1);
+		
+		public function ping($hostname, $echoRequestCount = 3) {
+
+			exec('ping -c '.$echoRequestCount.' '.$hostname." 2>&1",$this->pingResult['rawStdout'],$this->pingResult['returnVar']);
+		}
 		
 	}
--- test_ping.php
+++ test_ping.php
@@ -2,6 +2,8 @@
 
 	require_once ('./ping/ping.class.php');
 	
-	$p = new ping();
+	$p = new ping('localhost',1);
 	
+	if ($p->pingResult['returnVar'] == 0) echo "Success\n"; else echo "False";
+		
 	
