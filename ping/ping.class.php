6f542cde6913e2bf0aa6056078b0f1a5b4b56c7d
Anton Petrov <bugzzilla@gmail.com>
2016-08-08 10:27:07 +0500

    rep update

--- ping/ping.class.php
+++ ping/ping.class.php
@@ -19,21 +19,17 @@
 		
 		public function ping($host = NULL, $echoRequestCount = NULL) {
 
-			$cmd = '';
-			
 			if ($host) $this->host = $host;
 			if ($echoRequestCount) $this->echoRequestCount = $echoRequestCount;			
 				
 			if ($this->host) {
-				if (strpos(strtoupper(PHP_OS), 'LINUX') == 0) {
-					$cmd = 'ping -c '.$this->echoRequestCount.' '.$this->host." 2>&1";
-				} elseif (strpos(strtoupper(PHP_OS), 'WIN') == 0) {
-					$cmd = 'ping -n '.$this->echoRequestCount.' '.$this->host;					
+				if (strpos(strtoupper(PHP_OS), 'LINUX') === 0) {
+					exec('ping -c '.$this->echoRequestCount.' '.$this->host." 2>&1",$this->pingResult['rawStdout'],$this->pingResult['returnVar']);					
+				} elseif (strpos(strtoupper(PHP_OS), 'WIN') === 0) {
+					exec('chcp 437',$this->pingResult['rawStdout'],$this->pingResult['returnVar']);					
+					exec('ping -n '.$this->echoRequestCount.' '.$this->host,$this->pingResult['rawStdout'],$this->pingResult['returnVar']);					
 				}
 			}
-			
-			echo $cmd.PHP_EOL;
-			if ($cmd) exec($cmd,$this->pingResult['rawStdout'],$this->pingResult['returnVar']);
 			
 		}
 		
