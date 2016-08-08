4f92736caec3def63eca23fd7f7f0ddcb58b8713
Anton Petrov <bugzzilla@gmail.com>
2016-08-08 09:52:12 +0500

    rep update

--- ping/ping.class.php
+++ ping/ping.class.php
@@ -2,20 +2,34 @@
 
 	class ping {
 		
+		public $host = '';
+		public $echoRequestCount = 3;
+		
 		public $pingResult = array (
 							'os' => '',
 							'rawStdout' => '', 
-							'returnVar' => -1);
+							'returnVar' => -1
+		);
 		
-		public function ping($hostname = NULL, $echoRequestCount = 3) {
+		public function __construct($host = NULL, $echoRequestCount = NULL) {
+			
+			if ($host) $this->host = $host;
+			if ($echoRequestCount) $this->echoRequestCount = $echoRequestCount;
+			
+		}
+		
+		public function ping($host = NULL, $echoRequestCount = NULL) {
 
-			if ($hostname) {
+			if ($host) $this->host = $host;
+			if ($echoRequestCount) $this->echoRequestCount = $echoRequestCount;			
+				
+			if ($this->host) {
 				if (strpos(strtoupper(PHP_OS), 'LINUX') == 0) {
-					exec('ping -c '.$echoRequestCount.' '.$hostname." 2>&1",$this->pingResult['rawStdout'],$this->pingResult['returnVar']);
+					exec('ping -c '.$this->echoRequestCount.' '.$this->host." 2>&1",$this->pingResult['rawStdout'],$this->pingResult['returnVar']);
 				} elseif (strpos(strtoupper(PHP_OS), 'WIN') == 0) {
-					exec('ping -n '.$echoRequestCount.' '.$hostname,$this->pingResult['rawStdout'],$this->pingResult['returnVar']);					
-				}
-			}
+					exec('ping -n '.$this->echoRequestCount.' '.$this->host,$this->pingResult['rawStdout'],$this->pingResult['returnVar']);					
+				} else return false;
+			}				
 		}
 		
 	}
